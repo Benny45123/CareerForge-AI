6323c606b2841fed408ecede3f5f775a3a8bb614
Benny Hinn <sunnyashish41@gmail.com>
2025-11-07 13:25:34 +0530

    Deployement issue fixes

--- Backend/app.js
+++ Backend/app.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
+const express = require("express");
+// const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require( "dotenv");
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 dotenv.config();
 const PORT = process.env.PORT;
-const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
-app.get('/', (req, res) => {
+// const MONGO_URI = process.env.MONGO_URI;
+// mongoose.connect(MONGO_URI).then(console.log("Connected to MongoDB")).catch((err) => console.log(err));
+app.get('/api', (req, res) => {
     res.send('API is running...');
 })
 module.exports =  app ;
