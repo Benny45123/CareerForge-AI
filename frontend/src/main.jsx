868b2bfc79b394250b7f211c44fdba8d019a38e3
Benny Hinn <sunnyashish41@gmail.com>
2025-11-20 21:56:57 +0530

    Home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import Auth from './pages/Auth.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-    <Auth/>
+    <App/>
     </BrowserRouter>
   </StrictMode>,
 )
